b96fe84943d300e7e81655f1c421b5a0509177f9
siva-007 <sivavignesh761@gmail,com>
2022-05-29 21:51:05 +0530

    ml model completed

---
+++ server.py
@@ -0,0 +1,25 @@
+from flask import Flask, jsonify, request
+from flask_restful import Api, Resource
+import joblib
+import pandas as pd
+
+app = Flask("Team_TNEB")
+
+loaded_model = joblib.load("model")
+loaded_vect = joblib.load("vectorizer")
+
+@app.route("/user/predict", methods=["POST"])
+def user():
+    body = request.get_json()
+    print(body)
+
+    l = [body["message"]]
+    df = pd.DataFrame(l, columns=["Query"])
+    
+
+    df = loaded_vect.transform(df["Query"])
+    prediction = loaded_model.predict(df)
+
+    return jsonify({ "prediction": str(prediction[0])})
+
+app.run(port=9090)
